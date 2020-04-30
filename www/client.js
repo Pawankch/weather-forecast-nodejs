41bd450e5b7a1fb9024635c3c2a121210eac739f
NorinMp143 <madanparmar637@gmail.com>
2020-05-01 03:06:12 +0530

    new interface applied

--- www/client.js
+++ www/client.js
@@ -1,16 +1,17 @@
 console.log('Client js')
 
-const add = document.querySelector('input')
+const add = document.querySelector('#location')
 const form = document.querySelector('form')
-const p = document.querySelector('p')
-p.textContent = 'Enter City Name to get the Temperature'
+const locate = document.querySelector('#locationDetailer')
+const tempdl = document.querySelector('#tempDetailer')
 
 form.addEventListener('submit', (e)=>{ 
     e.preventDefault()
     address = add.value
     fetch('/weather?address='+address).then((response)=>{
         response.json().then((data)=>{
-            p.textContent = 'Cuurently it is '+data.temp+' Degree Celcius in '+address
+            locate.textContent = address
+            tempdl.textContent = data.temp
             console.log(data.temp)
         })
     })
