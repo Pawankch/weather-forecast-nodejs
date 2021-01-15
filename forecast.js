4eb0dd575cbedcf0e27009b7e39ebe7f41447560
Pawan Chaudhary <pawan.chaudhary@zerovir.in>
2021-01-15 18:32:58 +0530

    Changed syntax with async awit

--- forecast.js
+++ forecast.js
@@ -1,22 +1,37 @@
 const request = require('request')
-const weatherfuntion = (Location, callback)=>
+const weatherfuntion = async(location, callback)=>
 {
-    const weatherstack = 'http://api.weatherstack.com/current?access_key=d0cf158aed30cf02ec82e542225816f6&query='+Location
-    request({url : weatherstack, json : true}, (err, res)=>{
-        if(err)
-        {
-            callback('Network is not available',  undefined)
-        }
-        else if(res.body.error)
-        {
-            callback('Location is not available', undefined)
-           // console.log(res.body.error)
-        }
-        else
-        {
-            callback(undefined, {temp : res.body.current.temperature})
-        }
-    })
+    const weatherUri = 'http://api.weatherstack.com/current?access_key=d0cf158aed30cf02ec82e542225816f6&query='+location
+    try {
+        const weatherDetails = await fetch(weatherUri)
+    } catch (error) {
+        console.log('Network Unavailable '+error)
+        var msg = 'Network Error'
+    }
+    if(res.body.error)
+    {
+        console.log('Location is not available')
+         msg = 'location is not available'
+        return;
+    }
+    return {Error:msg , temp:res.body.current.temperature }
 }
+    
+    // request({url : weatherstack, json : true}, (err, res)=>{
+    //     if(err)
+    //     {
+    //         callback('Network is not available',  undefined)
+    //     }
+    //     else if(res.body.error)
+    //     {
+    //         callback('location is not available', undefined)
+    //        // console.log(res.body.error)
+    //     }
+    //     else
+    //     {
+    //         callback(undefined, {temp : res.body.current.temperature})
+    //     }
+    // })
+
  module.exports = weatherfuntion;
 
